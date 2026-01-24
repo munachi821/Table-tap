06ccd845f06bbf6ec8312e36bb14f1c2e3855c45
Munachi <munaonye4@gmail.com>
2026-01-24 19:03:37 +0100

    made a few changes

--- app/kitchen/page.tsx
+++ app/kitchen/page.tsx
@@ -1,22 +1,22 @@
-import { Pizza, Settings, Utensils } from "lucide-react";
+import { Bell, Pizza, Settings, Utensils } from "lucide-react";
 
 const Kitchen = () => {
   return (
     <main className="">
       <div className="flex bg-slate-50">
         {/* Sidebar */}
-        <div className="border-r border-white pt-3.5 bg-white w-68 shrink-0">
+        <div className="border-r border-white pt-2 bg-white w-68 shrink-0">
           <div className="flex gap-2 items-end pl-3">
             <div className="border border-orange-200 size-12 rounded-lg bg-white"></div>
             <div className="font-semibold">
-              <p className="text-lg text-gray-700 leading-3">
+              <p className="text-lg text-gray-700 leading-4">
                 Kitchen Dashboard
               </p>
               <p className="text-sm text-slate-500">Chicken Republic</p>
             </div>
           </div>
 
-          <div className="border-t border-gray-200 mt-4 pt-4 px-3 flex flex-col gap-2">
+          <div className="border-t border-gray-200 mt-3 pt-4 px-3 flex flex-col gap-2">
             <div className="flex px-1 py-1.5 gap-2 items-center border-orange-400 bg-orange-50 transition-colors rounded-lg">
               {/* if active then bg-orange-500 text-white */}
               <div className="h-8 w-12 rounded-md flex items-center justify-center bg-orange-400 transition-colors text-white ml-2">
@@ -37,7 +37,7 @@ const Kitchen = () => {
         {/* Kitchen view */}
         <div className="w-full h-screen">
           {/* filter orders in the header */}
-          <header className="flex justify-between bg-white px-4 py-4.5 items-center">
+          <header className="flex justify-between bg-white px-4 py-3.5 items-center">
             <div className="flex items-center gap-3">
               <h2 className="text-lg font-semibold">Orders</h2>
               <p className="bg-orange-400/90 px-3 font-semibold py-1 rounded-full text-white">
@@ -48,6 +48,9 @@ const Kitchen = () => {
             <div className="flex gap-2 items-center">
               <div className="p-2 bg-white rounded-full border border-gray-200">
                 <Settings />
+              </div>
+              <div className="p-2 bg-white rounded-full border border-gray-200">
+                <Bell />
               </div>
               <div className="flex gap-3 bg-white py-1 px-2 rounded-lg border border-gray-200 w-fit">
                 <button className="px-6 py-1.5 rounded-full text-sm font-semibold capitalize transition-all bg-slate-600 text-white shadow-sm">
