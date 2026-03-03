55762c4bf408e1d1e914e42c422616c47bd5f664
Munachi <munaonye4@gmail.com>
2026-03-03 18:01:48 +0100

    got the card UI

--- app/kitchen/page.tsx
+++ app/kitchen/page.tsx
@@ -7,10 +7,23 @@ const Kitchen = () => {
         {/* Navbar */}
         <header className="flex justify-between bg-white px-4 py-3.5 items-center">
           <div className="flex items-center gap-3">
-            <h2 className="text-lg font-semibold">Orders</h2>
-            <p className="bg-orange-400/90 px-3 font-semibold py-1 rounded-full text-white">
-              3 Pending
-            </p>
+            <div className="flex items-end gap-2">
+              <div className="size-11 rounded-full border border-orange-100"></div>
+              <div>
+                <p className="font-semibold text-lg leading-4 text-orange-500">
+                  Nene&apos;s Pastries
+                </p>
+                <p className="text-xs font-semibold text-gray-500">
+                  Kitchen Dashboard
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-3 items-center">
+              <p className="bg-orange-400/90 px-3 font-semibold py-1 rounded-full text-white">
+                3 Pending
+              </p>
+              <h2 className="text-lg font-semibold">Orders</h2>
+            </div>
           </div>
           <div className="flex gap-4 items-center">
             <div className="font-semibold flex gap-2 text-lg border px-2.5 rounded-full border-gray-200 py-1">
@@ -52,15 +65,36 @@ const Kitchen = () => {
         <div className="p-4">
           <h2 className="font-semibold text-2xl text-gray-700 mb-2">Table 1</h2>
           <div className="grid grid-cols-4 gap-4">
-            <div className="h-20 rounded-xl border-2 border-gray-700 p-2 bg-white/50 backdrop-blur-xl">
-              <header className="flex items-center justify-between">
+            <div className="rounded-xl border-2 border-gray-700 p-2 bg-white/50 backdrop-blur-xl">
+              <header className="flex items-center justify-between border-b pb-2 mb-2 border-gray-300">
                 <div className="size-8 flex items-center justify-center text-white bg-orange-400/95 text-lg font-bold rounded-full">
                   1
                 </div>
-                <div className="py-4 text-white bg-orange-400/95 text-base leading-0 font-semibold rounded-full w-fit px-3">
+                <div className="py-4 text-white bg-orange-400/80 text-base leading-0 font-semibold rounded-full w-fit px-3">
                   1 min ago
                 </div>
               </header>
+
+              <div className="px-2">
+                <div className="flex justify-between font-semibold">
+                  <h3 className="text-gray-700">Food: Fried Rice</h3>{" "}
+                  <p className="text-orange-400">Qty: 2</p>
+                </div>
+                <p className="list-none text-orange-400 font-semibold">
+                  extra:{" "}
+                </p>
+                <ul className="list-disc pl-2 font-semibold">
+                  <li>2 extra beef</li>
+                  <li>2 extra beef</li>
+                  <li>2 extra beef</li>
+                </ul>
+              </div>
+
+              <div className="mt-2">
+                <button className="px-6 py-1.5 rounded-full text-base font-semibold capitalize transition-all hover:bg-slate-600 transition-all bg-slate-700 text-white shadow-base cursor-pointer">
+                  Order Completed
+                </button>
+              </div>
             </div>
           </div>
         </div>
