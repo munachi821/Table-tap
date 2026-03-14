ac6480b657c0576db1c07245ce1400f480227fa3
Munachi <munaonye4@gmail.com>
2026-03-14 22:38:21 +0100

    started working on the owner dashboard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,9 @@
 const Page = () => {
-  return <div>Admin Dashboard</div>;
+  return (
+    <main className="flex h-screen">
+      <div className="w-60 h-full bg-red-500 shrink-0"></div>
+      <div className="w-full h-full bg-orange-400"></div>
+    </main>
+  );
 };
 export default Page;
