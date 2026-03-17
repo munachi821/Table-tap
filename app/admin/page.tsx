aeaa3fcb0bf1c1df7e6f88e2441e4b17734bc59d
Munachi <munaonye4@gmail.com>
2026-03-17 15:40:33 +0100

    worked on the colors

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -106,10 +106,10 @@ const Page = () => {
 
   return (
     <main className="flex h-screen">
-      <div className="w-68 h-full shrink-0 border-r border-gray-100">
+      <div className="w-68 h-full shrink-0 border-r border-gray-100 bg-orange-400">
         <div className="p-4 flex gap-2.5 items-end border-b border-gray-100">
-          <div className="size-10 rounded-full border border-orange-200"></div>
-          <p className="text-xl text-gray-700 font-semibold">
+          <div className="size-10 rounded-full border border-orange-200 bg-white"></div>
+          <p className="text-xl text-white font-semibold">
             Nana&apos;s Kitchen
           </p>
         </div>
@@ -118,7 +118,7 @@ const Page = () => {
           <ul className="flex flex-col gap-2">
             {admintabs.map((tab) => (
               <li
-                className={`flex items-center gap-2 text-lg ${activeTab === tab.name ? "font-semibold text-orange-800 bg-[#FEF3C7]" : "text-[#64748B] hover:bg-gray-100/70"} p-2.5 rounded-md cursor-pointer transition-all`}
+                className={`flex items-center gap-2 text-lg ${activeTab === tab.name ? "font-semibold text-orange-800 bg-[#FEF3C7]/80" : "text-white hover:bg-gray-100/30"} p-2.5 rounded-md cursor-pointer transition-all`}
                 key={tab.name}
                 onClick={() => setActiveTab(tab.name)}
               >
